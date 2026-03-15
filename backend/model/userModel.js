66a64b9a56f65d5dfff03f8c504d0dc9efed7e4d
madiha <madihaelyazal29@gmail.com>
2026-03-15 06:06:22 +0200

    fix error

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,18 +1,21 @@
-import { hashSync } from 'bcrypt';
 import db from '../config/data.js';
-export const createUser=(name,email,phone,hashedPassword,role = "customer", callback)=>{
- const sql="INSERT INTO users(name,email,password,role,phone) VALUES (?,?,?,?,?)";
- db.query(sql,[name,email,hashedPassword,role,phone],callback);
+
+export const createUser = (name, email, phone, hashedPassword, role = "customer", callback) => {
+  const sql = "INSERT INTO users(name,email,password,role,phone) VALUES (?,?,?,?,?)";
+  db.query(sql, [name, email, hashedPassword, role, phone], callback);
 };
-export const findUser=(email,callback)=>{
-  const sql="SELECT * FROM users WHERE email=?";
-  db.query(sql,[email],callback);
+
+export const findUser = (email, callback) => {
+  const sql = "SELECT * FROM users WHERE email=?";
+  db.query(sql, [email], callback);
 };
+
 export const verifyUserEmail = (email, callback) => {
   const sql = "UPDATE users SET is_verified = TRUE WHERE email = ?";
   db.query(sql, [email], callback);
 };
-export const updatePassword=(email,hashedPassword,callback)=>{
-  const sql="UPDATE users SET password=? WHERE email =?";
-  db.query(sql,[email,hashedPassword],callback);
+
+export const updatePassword = (email, hashedPassword, callback) => {
+  const sql = "UPDATE users SET password = ? WHERE email = ?";
+  db.query(sql, [hashedPassword, email], callback); 
 };
